f08e9675a9fb3d93a94093b9beee993707554c75
callumgran <callum.gran@gmail.com>
2023-10-15 02:46:38 +0200

    init commit

--- include/common.h
+++ include/common.h
@@ -45,17 +45,4 @@
 
 #define NORMALIZE(X) NORMALIZE_##X
 
-#define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
-#define PRIMITIVE_CAT(a, ...) a##__VA_ARGS__
-
-#define CHECK_N(x, n, ...) n
-#define CHECK(...) CHECK_N(__VA_ARGS__, 0, )
-#define PROBE(x) x, 1,
-
-#define IS_PAREN(x) CHECK(IS_PAREN_PROBE x)
-#define IS_PAREN_PROBE(...) PROBE(~)
-
-#define PRIMITIVE_COMPARE(x, y) IS_PAREN(COMPARE_##x(COMPARE_##y)(()))
-
-
 #endif // COMMON_H
